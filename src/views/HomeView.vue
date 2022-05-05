f081b9b8e0addaaa426da63c46834beed6ab1fac
DidiRoyadi123 <didiroyadi123@gmail.com>
2022-05-05 14:38:58 +0700

    perbaikan bug hitung dan insert bmi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,16 +8,15 @@
       <br />
       <br />
 
-      <form action="submit" >
+      <form action="submit">
         <input
           type="number"
           placeholder="Berat Badan"
           required
           v-model="berat"
           id="berat"
-        /> kg 
-        
-        ||
+        />
+        kg ||
 
         <input
           type="number"
@@ -25,17 +24,22 @@
           required
           v-model="tinggi"
           id="tinggi"
-        />cm 
-        
-        <br />
-        <br />
+        />cm
 
+        <br />
+        <br />
       </form>
-      <button @click="hitungBmi" type="submit" :disabled="!tinggi && !berat && !nama">
+      <button
+        @click="hitungBmi"
+        type="submit"
+        :disabled="!nama || !berat || !tinggi"
+      >
         Hitung BMI
       </button>
 
-      <p v-if="this.bmi != 0">Nilai BMI anda adalah : {{ Math.round(this.bmi) }}</p>
+      <p v-if="this.bmi != 0">
+        Nilai BMI anda adalah : {{ Math.round(this.bmi) }}
+      </p>
 
       <p>{{ this.statusnya }}</p>
 
@@ -90,105 +94,113 @@ export default {
       riwayat: "",
       berat: "",
       tinggi: "",
-      rekomendasi: "",
-      simpan: [],
+      // rekomendasi: "",
     };
   },
   methods: {
-
     hitungBmi() {
-              
-              this.tinggi /= 100;
-              this.bmi = this.berat / (this.tinggi * this.tinggi);
-              if (this.bmi <= 18.5) {
-                this.statusnya = "Berat badan di bawah Normal ";
-              } else if (this.bmi <= 24.9) {
-                this.statusnya = "Ideal";
-              } else if (this.bmi <= 29.9) {
-                this.statusnya = "Berat Badan Berlebih";
-              } else {
-                this. statusnya = "Obesitas";
-              }
-              return Math.round(this.bmi);
+      
+      this.bmi = this.berat / ((this.tinggi / 100) ** 2);
+      if (this.bmi <= 18.5) {
+        this.statusnya = "Berat badan di bawah Normal ";
+      } else if (this.bmi <= 24.9) {
+        this.statusnya = "Ideal";
+      } else if (this.bmi <= 29.9) {
+        this.statusnya = "Berat Badan Berlebih";
+      } else {
+        this.statusnya = "Obesitas";
+      }
     },
-    
+
     simpanData() {
+      this.nomor = this.nomor + 1;
       this.$apollo.mutate({
         mutation: gql`
-          mutation simpanData( $nama: String = "", $bmi:  numeric = "" , $statusnya: String = "") {
-           insert_riwayat(objects: {nama: $nama, bmi: $bmi, statusnya: $statusnya}) {
+          mutation simpanData(
+            $nama: String = ""
+            $bmi: numeric = ""
+            $statusnya: String = ""
+          ) {
+            insert_riwayat(
+              objects: { nama: $nama, bmi: $bmi, statusnya: $statusnya }
+            ) {
               returning {
-                      id
-                      created_at
-                      nama
-                      bmi
-                      statusnya
+                id
+                created_at
+                nama
+                bmi
+                statusnya
               }
             }
           }
         `,
         variables: {
           nama: this.nama,
-          bmi: this.bmi,
-          statusnya: this. statusnya,
-         
+          bmi: Math.round(this.bmi),
+          statusnya: this.statusnya,
         },
-        update : (store, { data: { insert_riwayat } }) => {
-          const data = store.readQuery({ query: gql`
-            query getRiwayat {
-              riwayat {
-                id
-                created_at
-                nama
-                bmi
-                statusnya
+        update: (store, { data: { insert_riwayat } }) => {
+          const data = store.readQuery({
+            query: gql`
+              query getRiwayat {
+                riwayat {
+                  id
+                  created_at
+                  nama
+                  bmi
+                  statusnya
+                }
               }
-            }
-          ` });
+            `,
+          });
           data.riwayat.push(insert_riwayat.returning[0]);
-          store.writeQuery({ query: gql`
-            query getRiwayat {
-              riwayat {
-                id
-                created_at
-                nama
-                bmi
-                statusnya
+          store.writeQuery({
+            query: gql`
+              query getRiwayat {
+                riwayat {
+                  id
+                  created_at
+                  nama
+                  bmi
+                  statusnya
+                }
               }
-            }
-          `, data });
+            `,
+            data,
+          });
         },
       });
     },
 
-      // ini untuk mutation insert query
-      //     mutation MyMutation($created_at: date = "", $nama: String = "", $bmi: Int = , $status: String = "") {
-      // insert_riwayat(objects: {created_at: $created_at, nama: $nama, bmi: $bmi, status: $status}) {
-      //   returning {
-      //     id
-      //     created_at
-      //     nama
-      //     bmi
-      //     status
-      //     }
-      //   }
+    // ini untuk mutation insert query
+    //     mutation MyMutation($created_at: date = "", $nama: String = "", $bmi: Int = , $status: String = "") {
+    // insert_riwayat(objects: {created_at: $created_at, nama: $nama, bmi: $bmi, status: $status}) {
+    //   returning {
+    //     id
+    //     created_at
+    //     nama
+    //     bmi
+    //     status
+    //     }
+    //   }
     // },
-      hapusData(id) {
-        this.$apollo.mutate({
-          mutation: gql`
-            mutation hapusData($id: Int) {
-              delete_riwayat(where: {id: {_eq: $id}}) {
-                returning {
-                  id
-                }
+    hapusData(id) {
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation hapusData($id: Int) {
+            delete_riwayat(where: { id: { _eq: $id } }) {
+              returning {
+                id
               }
             }
-          `,
-          variables: {
-            id: id,
-          },
-          update : (store, { data: { delete_riwayat } }) => {
-            const data = store.readQuery({ query: gql`
+          }
+        `,
+        variables: {
+          id: id,
+        },
+        update: (store, { data: { delete_riwayat } }) => {
+          const data = store.readQuery({
+            query: gql`
               query getRiwayat {
                 riwayat {
                   id
@@ -198,9 +210,13 @@ export default {
                   statusnya
                 }
               }
-            ` });
-            data.riwayat = data.riwayat.filter(riwayat => riwayat.id !== delete_riwayat.returning[0].id);
-            store.writeQuery({ query: gql`
+            `,
+          });
+          data.riwayat = data.riwayat.filter(
+            (riwayat) => riwayat.id !== delete_riwayat.returning[0].id
+          );
+          store.writeQuery({
+            query: gql`
               query getRiwayat {
                 riwayat {
                   id
@@ -210,12 +226,12 @@ export default {
                   statusnya
                 }
               }
-            `, data });
-          },
-        });
-      },
-
-
+            `,
+            data,
+          });
+        },
+      });
+    },
   },
 
   components: {},
@@ -224,6 +240,8 @@ export default {
       return this.$apolloData.data.riwayat;
     },
   },
+
+  // Ambil data pas awal masuk
   apollo: {
     riwayat: {
       query: gql`
@@ -240,9 +258,7 @@ export default {
       result: (result) => {
         return result.riwayat;
       },
-      
     },
-    // Mutation
   },
 };
 </script>
