7885d44fada73794636454a143ef05657065c086
DidiRoyadi123 <didiroyadi123@gmail.com>
2022-05-12 16:56:37 +0700

    perbaikan minor

--- src/views/calcKalori.vue
+++ src/views/calcKalori.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="calcKalori">
     <v-app>
       <v-btn color="#79b8af" elevation="3"  x-large to ="/"> Kembali</v-btn>
     <v-img src="@/assets/construction1.jpg" sizes="100%" position="cover"> </v-img>
